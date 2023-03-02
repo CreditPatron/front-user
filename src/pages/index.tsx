8cce500fa04b764211ba6aba88b517a60ef0d05c
pablomasior <pawel.masior@lotangroup.com>
2023-03-02 15:06:26 +0100

    prep for open application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Widget from "components/widget";
-import OpenFunding from "components/funders/open-funding";
+import ApplicationsOpen from "components/applications/open";
 import CountUp from "react-countup";
 import VisibilitySensor from "react-visibility-sensor";
 import SectionTitle from "../components/section-title";
@@ -25,13 +25,13 @@ const Index: React.FC = () => {
       <>
         <SectionTitle title="" subtitle="" />
         <Widget>
-          <section className="pb-2" id="stats">
+          <section className="pb-0" id="stats">
           <Title
-            title="Opened Startups funding"
-            description="Find best sturtups and own the business of the future"
+            title="Aktywne wnioski kredytowe"
+            description="Wybierz lub wyszukaj otwarty wniosek i zainwestuj"
             />
             <div className="max-w-6xl mx-auto">
-              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 flex justify-center">
+              <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 flex justify-center">
                 <div className="hover-up-5 flex py-4 wow animate__animated animate__fadeIn" data-wow-delay=".2s">
                   <div className="shrink-0 flex justify-center items-center bg-slate-50 text-blue-500 rounded-xl h-12 w-12 sm:h-20 sm:w-20 ">
                     <svg className="w-8 h-8" fill="none" stroke="#273370" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -45,7 +45,7 @@ const Index: React.FC = () => {
                     <span className="sm:text-2xl font-bold font-heading count"><CounterUp count={100} time={1} />
                                     </span>
                     {/* <span className="sm:text-2xl font-bold font-heading count">%</span> */}
-                    <p className="text-xs sm:text-base text-slate-800">Statsups funded</p>
+                    <p className="text-xs sm:text-base text-slate-800">Otwartych wniosków</p>
                   </div>
                 </div>
                 <div className="hover-up-5 flex w-1/2 lg:w-auto py-4 wow animate__animated animate__fadeIn" data-wow-delay=".2s">
@@ -60,8 +60,8 @@ const Index: React.FC = () => {
                     <span className="sm:text-2xl font-bold font-heading count">+ </span>
                     <span className="sm:text-2xl font-bold font-heading count"><CounterUp count={100} time={1} />
                                     </span>
-                    <span className="sm:text-2xl font-bold font-heading count"> mln $</span>
-                    <p className="text-xs sm:text-base text-slate-800">Funding applied</p>
+                    <span className="sm:text-2xl font-bold font-heading count"> mln PLN</span>
+                    <p className="text-xs sm:text-base text-slate-800">Wartość wniosków</p>
                   </div>
                 </div>
                 <div className="hover-up-5 flex w-1/2 lg:w-auto py-4 wow animate__animated animate__fadeIn" data-wow-delay=".2s">
@@ -73,18 +73,34 @@ const Index: React.FC = () => {
                   </div>
                   <div className="sm:py-2 sm:ml-6">
                     <span className="sm:text-2xl font-bold font-heading count">+ </span>
-                    <span className="sm:text-2xl font-bold font-heading count"><CounterUp count={10} time={1} />
+                    <span className="sm:text-2xl font-bold font-heading count"><CounterUp count={100} time={1} />
                                     </span>
-                    <p className="text-xs sm:text-base text-slate-800">Opened funding</p>
+                    <span className="sm:text-2xl font-bold font-heading count">% </span>
+                    <p className="text-xs sm:text-base text-slate-800">Zabespieczonia BFG</p>
                   </div>
-                </div>
+                </div>                
+                <div className="hover-up-5 flex w-1/2 lg:w-auto py-4 wow animate__animated animate__fadeIn" data-wow-delay=".2s">
+                  <div className="shrink-0 flex justify-center items-center bg-slate-50 text-blue-500 rounded-xl h-12 w-12 sm:h-20 sm:w-20 ">
+                    <svg className="w-8 h-8" fill="none" stroke="#273370" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path>
+                    </svg>
+                  </div>
+                  <div className="sm:py-2 sm:ml-6">
+                    <span className="sm:text-2xl font-bold font-heading count">+ </span>
+                    <span className="sm:text-2xl font-bold font-heading count"><CounterUp count={100} time={1} />
+                                    </span>
+                    <span className="sm:text-2xl font-bold font-heading count">% </span>
+                    <p className="text-xs sm:text-base text-slate-800">Weryfikacji bankowych</p>
+                  </div>
+                </div>  
               </div>
             </div>
           </section>
         </Widget>
         <Widget>
           <div className="w-full">
-            <OpenFunding />
+            <ApplicationsOpen />
           </div>
         </Widget>
       </>
